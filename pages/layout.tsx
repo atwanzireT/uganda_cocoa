0b4b3ece88a4b4e3746da8ac996c09cc387765c1
atwanzireT <tatwanzire@gmail.com>
2023-09-10 13:50:06 +0300

    fixing bugs

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,7 +1,5 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import "@/assets/css/style.css"
 
 const inter = Inter({ subsets: ['latin'] })
 
